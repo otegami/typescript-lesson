b7974827a3c76fd2331fb134721a8e3224b97365
otegami <a.s.takuya1026@gmail.com>
2021-01-30 19:48:17 +0900

    Never

--- riact_book/grammer.ts
+++ riact_book/grammer.ts
@@ -8,33 +8,42 @@
 // turquoise.name = 'Turquoise Blue'
 // turquoise.rgb = '03c1ff'
 
-interface Status {
-  level: number;
-  maxHP: number;
-  maxMP: number;
-  [attr: string]: number
-}
+// interface Status {
+//   level: number;
+//   maxHP: number;
+//   maxMP: number;
+//   [attr: string]: number
+// }
 
-const myStatus: Status = {
-  level: 99,
-  maxHP: 999,
-  maxMP: 999,
-  attach: 999,
-  defense: 999,
-};
+// const myStatus: Status = {
+//   level: 99,
+//   maxHP: 999,
+//   maxMP: 999,
+//   attach: 999,
+//   defense: 999,
+// };
 
-enum Pet {
-  Cat = 'Cat',
-  Dog = 'Dog',
-  Rabbit = 'Rabbit',
-}
+// enum Pet {
+//   Cat = 'Cat',
+//   Dog = 'Dog',
+//   Rabbit = 'Rabbit',
+// }
 
-let Tom: Pet = Pet.Cat;
-Tom = 'Hamster';
-Tom = 'Dog';
-Tom = Pet.Dog;
+// let Tom: Pet = Pet.Cat;
+// Tom = 'Hamster';
+// Tom = 'Dog';
+// Tom = Pet.Dog;
 
-let Marry: 'Cat' | 'Dog' | 'Rabbit' = 'Cat';
-Marry = 'Rabbit';
-Marry = 'Parrot';
+// let Marry: 'Cat' | 'Dog' | 'Rabbit' = 'Cat';
+// Marry = 'Rabbit';
+// Marry = 'Parrot';
 
+// ## tuple
+// const userAttrs: [number, string, boolean] = [1, 'patty', true];
+// const [id, username, isAdmin] = userAttrs
+// console.log(id, username, isAdmin);
+
+const str = `{ "id": 1, "username": "patty" }`;
+const user = JSON.parse(str);
+
+console.log(user.id, user.address.zipCode);
